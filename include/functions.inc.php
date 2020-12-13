517fc2612397b878a0ebecc56b1b3688d149073b
siv2r <siv2ram@gmail.com>
2020-12-13 16:59:25 +0530

    fixed loginFunctions

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -133,53 +133,6 @@ function createAdmin($conn, $uname, $email, $hosp_id, $pswd) {
   exit();
 }
 
-function emptyInputLogin($uid, $pswd) {
-  if (empty($uid) || empty($pswd)) {
-    return true;
-  }
-  else {
-    return false;
-  }
-}
-
-function loginUser($conn, $uid, $pswd) {
-  // the row from the database is assigned to $userExists as an associative array
-  $userExists = UnameExists($conn, $uid, $uid);
-
-  if(!$userExists) {
-    header("location: ../pages/login.php?error=invalidUsername");
-    exit();
-  }
-
-  $pswdHashed = $userExists['password'];
-  $checkPswd = password_verify($pswd, $pswdHashed);
-
-  if($checkPswd === false) {
-    header("location: ../pages/login.php?error=invalidPassword");
-    exit();
-  }
-
-  else if ($checkPswd === true) {
-    session_start();
-
-    $_SESSION['userId'] = $userExists['username'];
-    $_SESSION['userType'] = $userExists['type'];
-    $_SESSION['hospId'] = $userExists['hosp_id'];
-    $_SESSION['userEmail'] = $userExists['email'];
-
-    if($userExists['hosp_id'] !== 0) { // get the hospital data if the user is not an Admin
-      
-      //this contains hospital data as an associative array
-      $_SESSION['userHospital'] = getHospitalById($conn, $userExists['hosp_id']);
-    }
-
-    //redirect to index page
-    header("location: ../index.php");
-    exit();
-  }
-
-}
-
 function getHospitals($conn) {     //returns all hospitals in database as associative array
   $query = "SELECT * FROM hospitals;";
   $result = mysqli_query($conn, $query);
--- pages/login/include/login.inc.php
+++ pages/login/include/login.inc.php
@@ -5,7 +5,8 @@ if(isset($_POST["submit"])){
   $pswd = $_POST["pswd"];
 
   require_once("../../../include/dbConnect.inc.php");
-  require_once("../../../include/functions.inc.php");
+  // require_once("../../../include/functions.inc.php");
+  require_once("loginFunctions.inc.php");
 
   if(emptyInputLogin($uid, $pswd) !== false) {
     header("Location: ../login.php?error=emptyInputLogin");
