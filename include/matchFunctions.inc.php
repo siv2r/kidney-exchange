144b7dd1d23e115771b16b9f8ace1de1a25fc494
siv2r <siv2ram@gmail.com>
2021-01-24 13:27:03 +0530

    finished match function

--- include/matchFunctions.inc.php
+++ include/matchFunctions.inc.php
@@ -1,4 +1,4 @@
-$<?php 
+<?php 
 
 function getPairDataById ($conn, $pair_id) {
 
@@ -64,11 +64,9 @@ function getPairDataById ($conn, $pair_id) {
     echo "givenPairId database query error" . mysqli_error($conn);
     exit();
   }
-
   $pairData = mysqli_fetch_assoc($result);
 
   return $pairData;
-
 }
 
 function getAllPairData($conn) {
@@ -133,7 +131,6 @@ function getAllPairData($conn) {
     echo "Database joining query error ". mysqli_error($conn);
     exit();
   }
-
   $pairDataArray = mysqli_fetch_all($queryResult, MYSQLI_ASSOC);
 
   return $pairDataArray;
@@ -170,35 +167,27 @@ function getAllowedPatientBgrp ($donorBgrp) {
   return $allowedPatientBgrp;
 }
 
-// return allowed donor blood group as array
-function getAllowedDonorBgrp ($patientBgrp) {
-  $allowedDonorBgrp = []; //result array
+function isValidBloodDonate($donor, $patient) {
+  $allowedPatientBgrp = getAllowedPatientBgrp($donor['dBGrp']);
+  $currentPatientBGrp = $patient['pBGrp'];
+  if (in_array($currentPatientBGrp, $allowedPatientBgrp)) {
+    return true;
+  } 
 
-  //remove Rh factor i.e, A +ve -> A
-  $patientBgrpNoRh = explode(' ', $patientBgrp)[0];
+  return false;
+}
 
-  //patient's own blood grp is allowed (both +ve and -ve)
-  //don't forget sapce in ' +ve'
-  array_push($allowedDonorBgrp, $patientBgrpNoRh . ' +ve'); 
-  array_push($allowedDonorBgrp, $patientBgrpNoRh . ' -ve');
+function isUApresent($donor, $patient) {
+  $donorHla = explode(", ", $donor['dHla']);
+  $patientUa = explode(", ", $patient['pUA']);
+  $result = array_intersect($donorHla, $patientUa);
 
-  // A or B can recieve blood from O
-  if($patientBgrpNoRh == 'A' || $patientBgrpNoRh == 'B') {
-    array_push($allowedDonorBgrp, 'O +ve');
-    array_push($allowedDonorBgrp, 'O -ve');
+  // donor has patient's unacceptable antigen
+  if (!empty($result)) { 
+    return true;
   }
 
-  // AB can recieve blood from O, A, B
-  else if($patientBgrpNoRh == 'AB') {
-    array_push($allowedDonorBgrp, 'O +ve');
-    array_push($allowedDonorBgrp, 'O -ve');
-    array_push($allowedDonorBgrp, 'A +ve');
-    array_push($allowedDonorBgrp, 'A -ve');
-    array_push($allowedDonorBgrp, 'B +ve');
-    array_push($allowedDonorBgrp, 'B -ve');
-  }
-
-  return $allowedDonorBgrp;
+  return false;
 }
 
 function filterHLA($givenHLA) {
@@ -250,122 +239,20 @@ function findPairScore($inputPair, $matchedPair) {
   return $pairScore;
 }
 
-function getMatches1 ($conn, $pair_id) {
-
-  // get the data of given pair
-  $inputPair = getPairDataById($conn, $pair_id);
-
-  $allowedPatientBgrp = getAllowedPatientBgrp($inputPair['donorBloodGroup']);
-  $allowedDonorBgrp = getAllowedDonorBgrp($inputPair['patientBloodGroup']);
-
-  // covert to comma separated strings for sql query
-  // is of form-----A +ve', 'A -ve', 'O +ve', 'O -ve -----final open and close quotes is given in the sql query
-  $allowedPatientBgrpStr = implode("', '", $allowedPatientBgrp);
-  $allowedDonorBgrpStr   = implode("', '", $allowedDonorBgrp);
-
-  // gives records with matching blood group
-  $possibleMatchQuery = 
-  "SELECT patients.blood_group AS patientBloodGroup, 
-  donors.blood_group AS donorBloodGroup, 
-  patients.name AS patientName, 
-  donors.name AS donorName, 
-  patients.dob AS patientDOB, 
-  donors.dob AS donorDOB, 
-  patients.sex AS patientSex, 
-  donors.sex AS donorSex, 
-  patients.ua_antigens AS patientUA, 
-  patients.hla_antigens AS patientHLA, 
-  donors.hla_antigens AS donorHLA, 
-  pd_pairs.pair_id AS pairId
-  FROM ((pd_pairs 
-  INNER JOIN patients ON pd_pairs.patient_id = patients.id) 
-  INNER JOIN donors ON pd_pairs.donor_id = donors.id)
-  WHERE patients.blood_group IN ('$allowedPatientBgrpStr') 
-  AND donors.blood_group IN ('$allowedDonorBgrpStr')";
-
-  $queryResult = mysqli_query($conn, $possibleMatchQuery);
-  if(!$queryResult) {
-    echo "Database possibleMatchQuery error ". mysqli_error($conn);
-    exit();
-  }
-
-  $matchedPairArray = mysqli_fetch_all($queryResult, MYSQLI_ASSOC);
-  $matchResults = array();
-
-  foreach ($matchedPairArray as $key => $matchedPair) {
-
-    //delete the record containing the same pairId
-    if ($matchedPair['pairId'] == $inputPair['pairId']) {
-      unset($matchedPairArray[$key]);
-      continue;
-    }
-
-    // (P_inp, D_inp)     ---> patient and donor of input pair i.e, $inputPair 
-    // (P_match, D_match) ---> patient and donor of a possible match i.e, $matchedPair
-
-    // check unacceptable antigens for P_inp & D_match
-    $inputPatientUA = explode(", ", $inputPair['patientUA']);
-    $matchedDonorHLA = explode(", ", $matchedPair['donorHLA']); 
-    if (array_intersect($inputPatientUA, $matchedDonorHLA)) {
-      unset($matchedPairArray[$key]);
-      continue;
-    }
-    
-    // check unacceptable antigens for P_match & D_inp
-    $matchedPatientUA = explode(", ", $matchedPair['patientUA']);
-    $inputDonorHLA = explode(", ", $inputPair['donorHLA']);
-    if (array_intersect($inputDonorHLA, $matchedPatientUA)) {
-      unset($matchedPairArray[$key]);
-      continue;
-    }
-
-    // Now both blood matches and DSA not present
-    // finding the pair score of input pair and matched pair
-    $pairScore = findPairScore($inputPair, $matchedPair);
-
-
-    $validPair = 
-    array(
-      "pairId" => $matchedPair['pairId'], 
-      "patientSex" => $matchedPair['patientSex'], 
-      "donorSex" => $matchedPair['donorSex'],
-      "patientDOB" => $matchedPair['patientDOB'],
-      "donorDOB" => $matchedPair['donorDOB'],
-      "patientBloodGroup" => $matchedPair['patientBloodGroup'],
-      "donorBloodGroup" => $matchedPair['donorBloodGroup'],
-      "patientHLA" => $matchedPair['patientHLA'],
-      "donorHLA" => $matchedPair['donorHLA'],
-      "pairScore" => $pairScore 
-      //pairScore[0] -> P_inp&D_match
-      //pariScore[1] -> P_match&D_inp
-      //pariScore[2] -> (P_inp&D_match) + (P_match&D_inp)
-    );
-
-    array_push($matchResults, $validPair);
-  }
-
-  // sort all the valid pair in descending order
-  function cmp($a, $b) {
-    if ($a['pairScore'][2] == $b['pairScore'][2]) {
-      return 0;
-    }
-    return ($a['pairScore'][2] > $b['pairScore'][2]) ? -1 : 1;
-  }
-
-  usort($matchResults, "cmp");
-
-  return $matchResults;
-}
-
 function calcScore($donor, $patient){
   return 1;
 }
 
 function isMatch($donor, $patient){
   // Can donor donate blood to patient?
-
+  if (!isValidBloodDonate($donor, $patient)) {
+    return false;
+  }
   // prescence of unacceptable antigen
-
+  elseif (isUApresent($donor, $patient)) {
+    return false;
+  }
+  
   return true;
 }
 
