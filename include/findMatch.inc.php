69eb60fcfb14100acf3e63c8cbaef29056233a89
siv2r <siv2ram@gmail.com>
2021-02-22 17:28:22 +0530

    match -> pairwiseMatch

--- include/findMatch.inc.php
+++ include/findMatch.inc.php
@@ -2,35 +2,35 @@
 session_start();
 
 if (isset($_POST['id'])) {
-    //connection to database and custom functions
-    require_once "../templates/db-connect.php";
-    require_once "../include/functions.inc.php";
-    require_once "../include/matchFunctions.inc.php";
-    require_once "../functions/scoring.func.php";
+  //connection to database and custom functions
+  require_once "../templates/db-connect.php";
+  require_once "../include/functions.inc.php";
+  require_once "../include/matchFunctions.inc.php";
+  require_once "../functions/scoring.func.php";
 
-    $pair_id = $_POST['id'];
+  $pair_id = $_POST['id'];
 
-    // check if pair id is valid or not
-    if (isValidPairId($pair_id) == false) {
-        header("location: ../pages/match.php?error=invalidPairId");
-        exit();
-    }
+  // check if pair id is valid or not
+  if (isValidPairId($pair_id) == false) {
+    header("location: ../pages/pairwiseMatch.php?error=invalidPairId");
+    exit();
+  }
 
-    // check if pair id is present in database
-    if (getPairById($conn, $pair_id) == false) {
-        header("location: ../pages/match.php?error=noPairIdExists");
-        exit();
-    }
+  // check if pair id is present in database
+  if (getPairById($conn, $pair_id) == false) {
+    header("location: ../pages/pairwiseMatch.php?error=noPairIdExists");
+    exit();
+  }
 
-    //check if the transplant coordinator is searching in the same hospital
-    $checkHospid = explode('-', $pair_id);
-    if ($_SESSION['userType'] === "Transplant coordinator" && $_SESSION['hospId'] != $checkHospid[0]) {
-        header("location: ../pages/match.php?error=notSameHosp");
-        exit();
-    }
+  //check if the transplant coordinator is searching in the same hospital
+  $checkHospid = explode('-', $pair_id);
+  if ($_SESSION['userType'] === "Transplant coordinator" && $_SESSION['hospId'] != $checkHospid[0]) {
+    header("location: ../pages/pairwiseMatch.php?error=notSameHosp");
+    exit();
+  }
 
-    $givenPairData = getPairDataById($conn, $pair_id);
-    $matchResults = getMatches($conn, $pair_id);
+  $givenPairData = getPairDataById($conn, $pair_id);
+  $matchResults = getMatches($conn, $pair_id);
 } else {
-    header("location: ../pages/match.php");
+  header("location: ../pages/pairwiseMatch.php");
 }
