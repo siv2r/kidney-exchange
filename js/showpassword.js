9220d0a03093c324ffe2a95c32bebf5289c162a9
arpit456jain <111arpit1@gmail.com>
2021-04-09 12:49:08 +0530

    combine 2 functions into one

--- js/showpassword.js
+++ js/showpassword.js
@@ -1,38 +1,38 @@
-// for login
-function box2()
-{
- 
-  var password=document.getElementById("pswd");
-  var x=document.getElementById("box").checked;
-  if(x==true)
-  {
-    password.type="text";
-  }
-  else
-  {
-    password.type="password";
-    
-  }
-}
-
-
-// for sign up
+// for show passwords
 function box1()
 {
   
   var password=document.getElementById("pswd");
   var password2=document.getElementById("pswd2");
   var x=document.getElementById("box").checked;
-  if(x==true)
+  if (password2 == null)
   {
-    password.type="text";
-    password2.type="text";
-    
+  //for login
+    if(x==true)
+    {
+      password.type="text";
+    }
+    else
+    {
+      password.type="password";
+      
+    }
   }
   else
   {
-    password.type="password";
-    password2.type="password";
-    
+    // for sign up
+    if(x==true)
+    {
+      password.type="text";
+      password2.type="text";
+      
+    }
+    else
+    {
+      password.type="password";
+      password2.type="password";
+      
+    }
   }
+
 }
