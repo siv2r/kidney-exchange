61fe49d280e447ca9dcb93dc969835f7b70430c0
arpit456jain <111arpit1@gmail.com>
2021-04-28 23:01:04 +0530

    make the signup.php responsive

--- pages/signup.php
+++ pages/signup.php
@@ -1,3 +1,5 @@
+<!-- for bootstrap css -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 <style>
 body{
   background-image: url("https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77701389136.jpg");
@@ -7,11 +9,6 @@ body{
 
 form {
   background: rgb(0, 0, 0, 0.5);
-  width: 40%;
-  /* height: 86%; */
-  margin: auto;
-  margin-top: 30px;
-  padding: 15px 20px;
 }
 
 input{
@@ -48,8 +45,6 @@ img.avatar {
 }
 
 .form-elements{
-	width: 80%;
-	margin: auto;
 	text-align: center;
 }
 
@@ -72,7 +67,7 @@ img.avatar {
 /* for show password */
 #showpass
 {
-  width:12vw;
+  width:15vw;
   color:#fff;
   font-size: 22px;
   font-family: Arial;
@@ -82,7 +77,13 @@ img.avatar {
   width: 20px;
   height: 20px;
 }
-
+/* On screens that are 992px or less, set the background color to blue */
+@media screen and (max-width: 992px) {
+  #showpass
+  {
+    width:55vw;
+  } 
+}
 </style>
 
 <?php include("../templates/header.php"); ?>
@@ -91,7 +92,8 @@ img.avatar {
       <?php include("../templates/navBar.php") ?>
   </div>
 
-	<form action="../include/signup.inc.php" method="POST" id=signupForm>
+  <div class="container col-lg-4 co-sm-11 px-0">
+	<form action="../include/signup.inc.php" class="py-5 px-3" method="POST" id=signupForm>
 
     <div class="form-elements">
       <h3>SIGN UP</h3>
@@ -150,6 +152,8 @@ img.avatar {
     </div>
       	
   </form>
-
-  <script src="../js/showpassword.js"></script>  
+  </div>
+  <script src="../js/showpassword.js"></script>
+  <!-- for bootstrap js   -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 <?php include("../include/footer.inc.php") ?>
