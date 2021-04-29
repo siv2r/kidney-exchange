26fea5a9c0836f77f6fd1d0a26d528a7b5c28504
Sivaram D <siv2ram@gmail.com>
2021-04-29 21:11:30 +0530

    Revert the home page back to the red themed version
    This reverts commit 472eab8f226ab580c1f046f76b7da7a99156645a.

--- index.php
+++ index.php
@@ -6,24 +6,12 @@
 
   body{
     /* background-image: url("https://images.pexels.com/photos/48604/pexels-photo-48604.jpeg?cs=srgb&dl=pexels-negative-space-48604.jpg&fm=jpg"); */
-    background-image: url("https://images.pexels.com/photos/3882464/pexels-photo-3882464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" );
-    background-size: cover; 
-       
-  }
-
-  .content{
-    text-align: center;
-    color: white;
-    font-size: 20px;  
-    margin: 100px;
-  }
-
-  .content h1{
-    text-align:center;    
+    background-image: url("https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77701388996.jpg" );
+    background-size: cover;
   }
 
   /* .header-img{
-    background-image: url("https://images.pexels.com/photos/48604/pexels-photo-48604.jpeg?cs=srgb&dl=pexels-negative-space-48604.jpg&fm=jpg");
+    background-image: url("https://images.pexels.com/photos/48604/pexels-photo-48604.jpeg?cs=srgb&dl=pexels-negative-space-48604.jpg&fm=jpg"); 
     background-size: cover;
     background-position: top center;
     position: relative;
@@ -33,25 +21,16 @@
 
 </style>
 
-<?php include "templates/header.php"?>
+<?php include("templates/header.php") ?>
 
   <!-- <div class="header-img"> -->
     <div class="nav-container">
-      <?php include "templates/navBar.php"?>
+      <?php include("templates/navBar.php") ?>
     </div>
   <!-- </div> -->
 
-<div class="content">
-  <h1> <b><u>Project Overview</u></b></h1><br>
-  <h3><b> Due to some medical incompatibility many people cannot donate their kidney to their loved ones.
-    This problem is called the living donor kidney exchange problem, and in most places of India, doctors solve it manually.<br>This project aims to automate a part of this process
-    by providing a platform to facilitate automized inter-hospital kidney transplants.This platform allows interested hospitals to register.
-    The doctors from the registered hospitals can create their account and add their patients' (having kidney problems) medical details to this platform.
-    After a doctor completes this process, this platform provides two main features: <br><br>
-    1) An option to view only the essential details of a patient required for a kidney transplant. <br>
-    2) For a given patient, all suitable matches ranked from worst to best from all the registered hospitals are displayed.</b><br> 
-  </h3>
-</div>
+
+
 
 </body>
 </html>
