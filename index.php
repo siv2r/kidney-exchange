e14ce9380827b10cc4e1618dc5a464f72ab75222
arpit456jain <111arpit1@gmail.com>
2021-04-29 22:51:03 +0530

    make bg image responsive

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     /* background-image: url("https://images.pexels.com/photos/48604/pexels-photo-48604.jpeg?cs=srgb&dl=pexels-negative-space-48604.jpg&fm=jpg"); */
     background-image: url("https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77701388996.jpg" );
     background-size: cover;
+    background-repeat: no-repeat;
   }
   
   /* .header-img{
@@ -26,6 +27,12 @@
   }
   .content h1 {
     font-size:2.5rem;
+  }
+  body{
+    background-image: url("https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77701388996.jpg" );
+    background-size: cover;
+    height:100vh;
+    background-repeat: no-repeat;
   }
 }
 </style>
