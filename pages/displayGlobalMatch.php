952a33fd5f2cbc017311909bb5c0f373161eedd6
arpit456jain <111arpit1@gmail.com>
2021-05-19 15:41:08 +0530

    fixed the footer

--- pages/displayGlobalMatch.php
+++ pages/displayGlobalMatch.php
@@ -5,7 +5,10 @@
     background-repeat: no-repeat;
     background-size: cover;
   }
-
+  #graphimg
+  {
+    height:66vh;
+  }
 
 </style>
 
@@ -13,9 +16,9 @@
   <div class="nav-container">
     <?php include "../templates/navBar.php";?>
   </div>
-
+  <div class="container" id="graphimg">
   <img src="/kidney-exchange/global_match/result/graph.png" alt="Solution Image">
   <!-- <img src="/kidney-exchange/global_match/result/solution" alt="Global Match"> -->
-
+  </div>
 
 <?php include_once "../include/footer.inc.php";?>
