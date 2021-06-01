5a39378a21b3a191bad96ab2d014096c8191b97b
111arpit1 <55352601+arpit456jain@users.noreply.github.com>
2021-06-01 07:00:15 +0530

    added back to top in edit pair form (#255)

--- pages/editPairForm.php
+++ pages/editPairForm.php
@@ -17,6 +17,8 @@
   <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.2/dist/additional-methods.min.js"></script>
   <script src="../js/editPairForm.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../css/backToTop.css">
 </head>
 
 <style>
@@ -64,6 +66,7 @@
   margin:0 auto !important;
 }
 }
+
 </style>
 
 <link rel="stylesheet" href="../css/form-style.css">
@@ -83,6 +86,8 @@ if (isset($_GET['pair_id'])) {
 ?>
 
 <body>
+<!-- Back to top button -->
+<a id="button" style="text-decoration:none"></a>
 
   <div class="header-img">
     <div class="nav-container">
@@ -1006,6 +1011,7 @@ foreach ($relationValues as $value) {
 
     </form>
   </div>
-
+  
+<script src="../js/backToTop.js"></script>
   <?php
 require_once "../include/footer.inc.php";
