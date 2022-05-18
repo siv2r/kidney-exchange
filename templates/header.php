472ab81ba088fcdb511109c4756d4033963d4549
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-05-18 18:28:55 +0530

    Updated `Changes`

--- templates/header.php
+++ templates/header.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Kidney Exchange Program</title>
   <!-- bootstrap css -->
- <link rel="favicon" type="image/png" href="../images/favicon.png"/>
+  <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
 
